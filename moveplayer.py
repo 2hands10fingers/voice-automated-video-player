c008e38bbc0e0ef25f1a371808f9007764cc2d96
Tono <antonio.nogueras@yahoo.com>
2018-01-07 14:38:03 -0600

    Create moveplayer.py

---
+++ moveplayer.py
@@ -0,0 +1,16 @@
+from os import listdir, path
+from subprocess import call
+
+computer_name = 'antonionogueras'
+moveie_directory = 'Documents/Programming/Voicer/Movies'
+
+moviefolder = '/Users/{}/{}'.format(computer_name, moveie_directory)
+
+string = 'The lion king'
+
+for i in listdir(moviefolder):
+	title = i.split('.')
+	
+	if string.lower() == title[0].lower():
+    vlc = '/Applications/VLC.app/Contents/MacOS/VLC'
+		call(['open', '-a', vlc, path.join(moviefolder, i)])
